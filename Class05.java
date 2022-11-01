f71bfdd53ebe04c9b10468684cf796921b2f42c8
Skes98055 <91359104+Skes98055@users.noreply.github.com>
2022-11-01 21:50:22 +0800

    Add files via upload

---
+++ Class05.java
@@ -0,0 +1,27 @@
+class Caaa{
+    private int num;
+    public Caaa(int n){
+        num=n;
+    }
+    public int get(){
+        return num;
+    }
+    public  Caaa () {
+        
+    }
+}
+
+class Cbbb extends Caaa{
+    public  Cbbb (int n) {
+        super(n);
+    }
+    public void show(){
+        System.out.println("num="+get());
+    }
+}
+public class Class05{
+    public static void main(String[] args) {
+        Cbbb bb=new Cbbb(2);
+        bb.show();
+    }
+}
